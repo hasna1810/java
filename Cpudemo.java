8f4575f1dc9a9011a4163faf95675f9220df2741
hasna1810 <121877642+hasna1810@users.noreply.github.com>
2025-03-16 20:56:36 +0530

    Add files via upload

--- Cpudemo.java
+++ Cpudemo.java
@@ -1,46 +1,58 @@
-class Cpu
-{
+import java.util.Scanner;
+class CPU{
     int price;
-    public Cpu(int price){
+    public CPU(int price){
         this.price=price;
     }
     public void display(){
-        System.out.println("price:"+price);
+        System.out.println("price of the cpu is:"+price);
     }
     class Processor{
-        int no_cores;
-        String manufacturer;
-        public Processor(int no_cores, String manufacturer){
-            this.no_cores=no_cores;
-            this.manufacturer=manufacturer;
+        int core;
+        String man;
+        public Processor(int core,String man){
+            this.core=core;
+            this.man=man;
         }
-        public void displayinfo(){
-            System.out.println("number of cores:"+no_cores+", Manufacturer:"+manufacturer);
-
+        public void display(){
+            System.out.println("core of the processor is:"+core+"\n manufacture is:"+man);
         }
     }
-    static class RAM{
-        String memory;
-        String manu;
-        public RAM(String memory,String manu){
-            this.memory=memory;
-            this.manu=manu;
+   static class RAM{
+        String mem;
+        String mans;
+        public RAM(String mem,String mans){
+            this.mem=mem;
+            this.mans=mans;
     
         }
-        public void disp(){
-            System.out.println("memory:"+memory+",manufacturer:"+manu);
+        public void display(){
+            System.out.println("memory of the ram is:"+mem+"\n manufactured by:"+mans);
         }
-
     }
-
 }
-public class Cpudemo{
-    public static void main(String[] arg){
-        Cpu c=new Cpu(25000);
-        Cpu.Processor p=c.new Processor(5,"intel");
-        Cpu.RAM r=new Cpu.RAM("32GB","SAMSUNG");
+
+
+public class Cpudemo {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the price:");
+        int price=sc.nextInt();
+        CPU c=new CPU(price);
+        System.out.println("enter the core:");
+        int core=sc.nextInt();
+        System.out.println("enter the manufracture:");
+        String man=sc.next();
+        CPU.Processor p=c.new Processor(core,man);
+        System.out.println("enter the memory:");
+        String mem=sc.next();
+        System.out.println("enter the manufactured by:");
+        String mans=sc.next();
+        CPU.RAM r=new CPU.RAM(mem,mans);
         c.display();
-        p.displayinfo();
-        r.disp();
+        p.display();
+        r.display();
+
     }
+    
 }
