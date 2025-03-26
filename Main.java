f4fde8f71041e7280c736a3b0e915433b1a07fc8
hasna1810 <121877642+hasna1810@users.noreply.github.com>
2025-03-26 14:20:51 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,71 +1,75 @@
-import java.util.Scanner;
-interface Measurable{
-    public double area();
-    public double perimeter();
-}
-class Circle implements Measurable{
-    double radius;
-    public Circle(double radius){
-        this.radius=radius;
-    }
-    public double area(){
-        return Math.PI *radius*radius;
-    }
-    public double perimeter(){
-        return 2*Math.PI*radius;
+class Publisher{
+    String pname;
+    String paddress;
+    public Publisher(String pname,String paddress){
+        this.pname=pname;
+        this.paddress=paddress;
     }
     public void display(){
-        System.out.println("Radius:"+radius);
-        System.out.println("Area of the circle is:"+area());
-        System.out.println("Perimeter of the circle is:"+perimeter());
-    }
-    }
-
-
-class Rectangle implements Measurable{
-    double length;
-    double width;
-    public Rectangle(double length,double width){
-        this.length=length;
-        this.width=width;
-    }
-    public double area(){
-        return length*width;
-    }
-    public double perimeter(){
-        return 2*(length+width);
-    }
-    public void display(){
-        System.out.println("length:"+length+"  width:"+width);
-        System.out.println("Area of the rectangle is:"+area());
-        System.out.println("Perimeter of the rectangle is:"+perimeter());
+        System.out.println("publisher name:"+pname+"\t address:"+paddress);
     }
 }
-public class Main{
+class Book extends Publisher implements Runnable{
+    String bname;
+    String author;
+    public Book(String pname,String paddress,String bname,String author){
+        super(pname,paddress);
+        this.bname=bname;
+        this.author=author;
+    }
+
+    public void run(){
+        System.out.println("book is being processed:"+bname);
+            display();
+            System.out.println("book name:"+bname+"\t author:"+author);
+        }
+
+    }
+class Department{
+    String dname;
+    public Department(String dname){
+        this.dname=dname;
+    }
+    public void display(){
+        System.out.println("department name:"+dname);
+    }
+}
+class Employee extends Department implements Runnable{
+    String ename;
+    String eaddress;
+    public Employee(String dname,String ename,String eaddress){
+        super(dname);
+        this.ename=ename;
+        this.eaddress=eaddress;
+    }
+    public void run(){
+        System.out.println("employee is being processed:"+ename);
+        display();
+        System.out.println("employee name:"+ename+"\t address:"+eaddress);
+
+    }
+}
+
+
+public class Main {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        
-        System.out.print("Enter the radius of circle:");
-        double radius=sc.nextDouble();
+        Book book=new Book("DC books", "kochi", "beauty and the beast","disney");
+        Employee em=new Employee("MCA", "ram", "palakkad");
 
-        Circle c=new Circle(radius);
-        c.display();
+        Thread t1=new Thread(book);
+        Thread t2=new Thread(em);
 
-        System.out.print("Enter length of the rectangle:");
-        double length=sc.nextDouble();
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        System.out.print("Enter width of the rectangle:");
-        double width=sc.nextDouble();
-
-        Rectangle r=new Rectangle(length,width);
-        r.display();
-
-        sc.close();
-
-
-
-
-
-
+        System.out.println("Processing completed for both book and employee.");
     }
-}
+    }
+    
+
