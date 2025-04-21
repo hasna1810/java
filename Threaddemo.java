dd21b4035e61b7d52c35321a91ee151e688373a6
hasna1810 <121877642+hasna1810@users.noreply.github.com>
2025-04-21 10:56:47 +0530

    Update Threaddemo.java

--- Threaddemo.java
+++ Threaddemo.java
@@ -31,29 +31,55 @@ class Multiplicationtable implements Runnable{
 
         }
     }
+class Prime implements Runnable{
+    public void run(){
+        System.out.println("Prime number thread is running:");
+        int n=20;
+        for (int i=2;i<=n;i++){
+            boolean isPrime= true ;
+            for(int j=2;j<i;j++){
+            if(i%j==0){
+                isPrime=false;
+                break;
+            }
+        }
+        if(isPrime){
+            System.out.println("prime number:"+i);
+        }
+        try{
+            Thread.sleep(100);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
 
+}}}
 
 public class Threaddemo{
     public static void main(String [] args){
         Multiplicationtable tabletask=new Multiplicationtable();
         Factorfind factortask=new Factorfind();
+        Prime primetask=new Prime();
 
         Thread tablethread=new Thread(tabletask);
         Thread factorthread=new Thread(factortask);
+        Thread primethread=new Thread(primetask);
 
         tablethread.start();
         factorthread.start();
+        primethread.start();
 
         System.out.println("main thread continues:");
         try{
             tablethread.join();
             factorthread.join();
+            primethread.join();
 
         }
         catch(InterruptedException e){
             e.printStackTrace();
         }
-        System.out.println("Both thread are finished");
+        System.out.println("threads are finished");
 
     }
-}
+}                                                                                                                                                                                                                      
